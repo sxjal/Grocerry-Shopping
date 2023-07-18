25b7900aaf5dd4776aa704c672788b7a852ae1ad
sajal <sajalsahu8@gmail.com>
2023-07-18 19:40:02 +0400

    added homnescreen

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,4 +1,6 @@
 import "package:flutter/material.dart";
+import "package:flutter_shopping_list/data/dummy_items.dart";
+//import "package:flutter_shopping_list/models/grocery_item.dart";
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -7,11 +9,26 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Groceries"),
+        title: const Text("Your Groceries"),
       ),
-      body: const Center(
-        child: Text("Hello World"),
-      ),
+      body: Center(
+          child: ListView.builder(
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: Container(
+              color: groceryItems[index].category.color,
+              width: 25,
+              height: 25,
+            ),
+            title: Text(
+              groceryItems[index].name,
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(),
+            ),
+            trailing: Text(groceryItems[index].quantity.toString()),
+          );
+        },
+        itemCount: groceryItems.length,
+      )),
     );
   }
 }
